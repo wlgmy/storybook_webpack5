9f4072033a9c02763ca490f39591bbac6e66f66f
wanglong01 <wanglong01@bilibili.com>
2021-07-05 17:58:54 +0800

    init

---
+++ .history/.storybook/main_20210615175734.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "core": {
+    "builder": "webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.pug$/,
+      use: ["pug-plain-loader"]
+    })
+    return config
+  }
+}
